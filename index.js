088a3f01f768850171f56da3bc7d22797e9c9b80
Lucas Templeton <templetl99@gmail.com>
2021-11-02 10:04:22 -0500

    commit tree af552cd1ccc27d437274d88b73ed098605525066

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (value1 = 2000, value2 = 100) => {
+   const answer = value1 / value2
+   return answer
+}
+const square = (value) => {
+    const answer = value * value
+    return answer
+}
+const add = (value1, value2) => {
+    const answer = value1 + value2
+    return answer
+}
